8a613bf2f978e1f7a91ebbb617678facf3a4d550
GonzaMB2 <gonzalombenavente@gmail.com>
2024-09-27 18:13:23 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ingreso</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+     <form action="Cargar.php" method="post">
+     	<h2>Proyecto</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>Ingrese las horas trabajadas</label>
+     	<input type="text" name="horas" placeholder="Horas semanales trabajadas"><br>
+
+     	<label>Ingrese el proyecto en proceso</label>
+     	<input type="text" name="proyecto" placeholder="proyecto"><br>
+      
+      <label>Ingrese el N°de trabajador</label>
+     	<input type="text" name="n de trabajador" placeholder="n de trabajador"><br>
+
+     	<button type="submit">Cargar horas</button>
+     </form>
+
+     <a href="Tabla.php">Mostrar</a>
+</body>
+</html> 
