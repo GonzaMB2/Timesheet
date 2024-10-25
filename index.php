4b75a0803a90115fbbb0a39129040f09ef402ee0
Matiasxd45 <loche474@gmail.com>
2024-10-25 14:22:43 -0600

    mejora

--- index.php
+++ index.php
@@ -1,23 +1,36 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Ingreso</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 </head>
+
 <body>
+
+	<div class="wrapper">
      <form action="Login.php" method="post">
-     	<h2>Inicio de sesion</h2>
+     	<h1>Inicio de sesion</h1>
      	<?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
      	<?php } ?>
-     	<label>Usuario</label>
-     	<input type="text" name="username" placeholder="Usuario"><br>
 
-     	<label>Contraseña</label>
-     	<input type="password" name="password" placeholder="password"><br>
+		<div class="input-box">
+     	<input type="text" name="username" 
+		placeholder="Usuario" required>
+		<i class='bx bxs-user'></i>
+		</div>
+
+     	<div class="input-box">
+     	<input type="password" name="password" 
+		placeholder="password" required>
+		<i class='bx bxs-lock-alt'></i>
+		</div>
       
-     	<button type="submit">Ingresar</button>
+     	<button type="submit" class="btn">Ingresar</button>
      </form>
-
+	 </div>
 </body>
 </html> 
